5fe247cf053b24d3115716488d721a1db4f39346
aashirnayyar <aashirnay54@gmail.com>
2026-01-30 12:20:44 -0800

    code

--- MyRobot_V1/MyRobot_V1.ino
+++ MyRobot_V1/MyRobot_V1.ino
@@ -1,93 +1,29 @@
-#include <WiFiS3.h>  // Correct library for Uno R4 WiFi
+/\*ESP Recovery sketch\*/
+#include <Wire.h>
+#include <UnoWiFiDevEd.h>
 
-const char* ssid     = "Aashir Nayyar's iPhone";
-const char* password = "Aashirnay54";
+#define BAUD 9600
+WifiData ESPSerial;
 
-int enA = 9;   // PWM pin for Motor A
-int in1 = 8;
-int in2 = 7;
+void setup()
+{
+Serial.begin(BAUD);
+pinMode(13, OUTPUT);
+digitalWrite(13, LOW);
 
-int enB = 5;   // PWM pin for Motor B
-int in3 = 4;
-int in4 = 2;
-
-
-void setup() {
-  Serial.begin(115200);
-
-  // --- Motor pins ---
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(enA, OUTPUT);
-
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
-  pinMode(enB, OUTPUT);
-
-  // --- Connect to Wi-Fi ---
-  Serial.print("Connecting to Wi-Fi...");
-  WiFi.begin(ssid, password);
-
-  if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("❌ CRITICAL ERROR: WiFi Module NOT detected!");
-    Serial.println("You MUST update the firmware (Tools > Firmware Updater).");
-    while (true); // Freeze here
-  }
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nConnected to Wi-Fi!");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
+ESPSerial.begin(BAUD);
 }
 
-
-void loop() {
-    // put your main code here, to run repeatedly:
-
-    if (Serial.available()) {
-        char c = Serial.read();
-
-        switch (c) {
-            case 'w':
-                moveForward(in1, in2, enA, in3, in4, enB);
-                Serial.println("FORWARD");
-                break;
-
-            case 's':
-                moveBackward(in1, in2, enA, in3, in4, enB);
-                Serial.println("BACKWARD");
-                break;
-
-            case 'a':
-                moveLeft(in1, in2, enA, in3, in4, enB);
-                Serial.println("LEFT");
-                break;
-
-            case 'd':
-                moveRight(in1, in2, enA, in3, in4, enB);
-                Serial.println("RIGHT");
-                break;
-
-
-            case 'q':
-                turnRobotInPlace(in1, in2, enA, in3, in4, enB);
-                Serial.println("IN PLACE");
-                break;
-
-            case 'e':
-                stop(in1, in2, in3, in4, enA, enB);
-                Serial.println("STOP");
-                break;
-         
-        }
-
-    }
-
-
-
-    
+void loop()
+{
+while (Serial.available()) {
+char inChar = (char)Serial.read();
+ESPSerial.write(inChar);
 }
 
+while (ESPSerial.available()) {
+char inChar = (char)ESPSerial.read();
+Serial.write(inChar);
+}
+
+}
